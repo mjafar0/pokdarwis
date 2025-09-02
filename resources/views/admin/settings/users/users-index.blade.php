e8a5d3ec71fd5cab53dd28bd951c0101a468d961
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2025-09-02 17:16:46 +0700

    users

--- resources/views/admin/settings/users/users-index.blade.php
+++ resources/views/admin/settings/users/users-index.blade.php
@@ -1,25 +1,44 @@
 @extends('layouts.app-backend')
 
 @section('page-header')
-    <h1>Users</h1>
+        <h1>Users</h1>
 @endsection
 
 @section('page-description')
-    Mengelola user superadmin
+        Mengelola user superadmin
 @endsection
+
+@section('page-styles')
+<link rel="stylesheet" href="{{ asset('assets/vendors/datatables/datatables.min.css') }}">
+@endsection
+
+
 @section('main')
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title">User Superadmin</h5>
-                </div>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">User Superadmin</h5>
             </div>
         </div>
+        <div class="card-body">
+            <table class="table table-sm">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Email</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
     </div>
+</div>
 @endsection
 
 @section('page-scripts')
-    
+<script src="{{ asset('assets/vendors/datatables/datatables.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/settings/settings-user.js') }}" defer></script>
 @endsection
 
